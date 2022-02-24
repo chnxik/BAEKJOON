8282ad0c2d25b7053a9f19d114707a894d0654cd
chnxik <chnxik@gmail.com>
2022-02-24 16:39:16 +0900

    백준 11021 A+B - 7

--- Study/main.cpp
+++ Study/main.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 
-// 2742¹ø  ±âÂï N
+// 11021¹ø A+B - 7
 
 int main()
 {
-	int N;
+	int N,A,B;
 	std::cin.tie(NULL);
 	std::cout.sync_with_stdio(false);
 	std::cin >> N;
-	for (int i = N; i > 0; --i) std::cout << i << '\n';
+	for (int i = 1; i <= N; ++i)
+	{
+		std::cin >> A >> B;
+		std::cout << "Case #"<< i <<": " << A+B << '\n';
+	}
 }
