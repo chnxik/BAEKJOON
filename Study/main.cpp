ffddb58f086636dd24d8ed8b15c0757192d26c14
chnxik <chnxik@gmail.com>
2022-02-24 18:18:24 +0900

    백준 10871번 X보다 작은 수

--- Study/main.cpp
+++ Study/main.cpp
@@ -1,18 +1,22 @@
 #include <iostream>
 
-// 2439번 별 찍기 - 2
-using std::cout;
+// 10871번 X보다 작은 수
 
 int main()
 {
-	int N;
-	std::cin.tie(NULL);
-	std::cout.sync_with_stdio(false);
-	std::cin >> N;
-	for (int i = 1; i <= N; ++i)
+	int N, X;
+	std::cin >> N >> X;
+	int* nums = new int[N];
+	bool ins = false;
+	for (int i = 0; i < N; ++i) std::cin >> nums[i];
+	for (int i = 0; i < N; ++i)
 	{
-		for (int j = 0; j < N - i; ++j) cout << ' ';
-		for (int k = 0; k < i; ++k) cout << '*';
-		cout << '\n';
+		if (nums[i] < X)
+		{
+			if (ins) std::cout << " ";
+			std::cout << nums[i];
+			ins = true;
+		}
 	}
+	delete[] nums;
 }
