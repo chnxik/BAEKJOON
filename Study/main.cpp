8bf72c020d0f11576dbff2038983f046685c00b0
chnxik <chnxik@gmail.com>
2022-02-24 22:26:00 +0900

    백준 10818번 최소, 최대

--- Study/main.cpp
+++ Study/main.cpp
@@ -1,16 +1,21 @@
 #include <iostream>
 
-// 1110번 더하기 사이클
+// 10818번 최소, 최대
+
+using std::cin;
 
 int main()
 {
-	short N,cnt = 0;
-	std::cin >> N;
-	short temp = N;
-	do
+	std::cout.sync_with_stdio(false);
+	std::cin.tie(NULL);
+	int N, temp;
+	cin >> N >> temp;
+	int max = temp, min = temp;
+	for (int i = 1; i < N; ++i)
 	{
-		N = (N % 10 * 10) + (N / 10 + N % 10) % 10;
-		++cnt;
-	} while (temp != N);
-	std::cout << cnt;
+		cin >> temp;
+		if (max < temp) max = temp;
+		if (min > temp) min = temp;
+	}
+	std::cout << min << ' ' << max;
 }
