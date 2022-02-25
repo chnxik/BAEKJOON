b9644a7e5c80eda2077f83b2dc368b021bc2be4d
chnxik <chnxik@gmail.com>
2022-02-25 17:46:41 +0900

    백준 8958번 OX퀴즈

--- Study/main.cpp
+++ Study/main.cpp
@@ -1,24 +1,34 @@
 #include <iostream>
 
-// 1546¹ø Æò±Õ
+// 8958¹ø OXÄûÁî
 
 using std::cin;
 
 int main()
 {
 	std::cin.tie(NULL);
+	std::cout.sync_with_stdio(false);
 
-	int N, score[1000] = { 0, }, M = 0;
-	double avg = 0;
+	int N,cnt,score;
+	char OX[80];
+	char* cp;
 	cin >> N;
 	for (int i = 0; i < N; ++i)
 	{
-		cin >> score[i];
-		if (M < score[i]) M = score[i];
+		cin >> OX;
+		cnt = 0;
+		score = 0;
+		cp = OX;
+		while (*cp)
+		{
+			if (*cp == 'O')
+			{
+				++cnt;
+				score += cnt;
+			}
+			else if (*cp == 'X') cnt = 0;
+			++cp;
+		}
+		std::cout << score << '\n';
 	}
-	for (int i = 0; i < N; ++i)
-	{
-		avg += (score[i] / (double)M) * 100;
-	}
-	std::cout << avg / N;
 }
