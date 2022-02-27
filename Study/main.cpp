d3d1cb98fc93e0c414fbe9a10ac68877648c0899
chnxik <chnxik@gmail.com>
2022-02-28 01:32:34 +0900

    백준 1712번 손익분기점

--- Study/main.cpp
+++ Study/main.cpp
@@ -1,41 +1,10 @@
 #include <iostream>
 
-// 1316번 그룹 단어 체커
-using std::cin;
-
-bool check_w(char* word)
-{
-	char* cp = word;
-	bool check = false;
-	do
-	{
-		if (check && *word == *cp) return false;
-		else if (*word != *cp) check = true;
-		++cp;
-	} while (*cp);
-	return true;
-}
+// 1712번 손익분기점
 
 int main()
 {
-	int N;
-	char word[101];
-	char* wp = word;
-	cin >> N;
-	int cnt = N;
-	while (N--)
-	{
-		cin >> word;
-		wp = word;
-		while (*wp)
-		{
-			if (!check_w(wp))
-			{
-				--cnt;
-				break;
-			}
-			++wp;
-		}
-	}
-	std::cout << cnt;
+	int A, B, C;
+	std::cin >> A >> B >> C;
+	std::cout << (B < C ? (A / (C - B)) + 1 : -1);
 }
