c4249a8497d1c3ed6a3348f35f1380bf6a6bfc8f
chnxik <chnxik@gmail.com>
2022-02-27 21:35:37 +0900

    백준 5622번 다이얼

--- Study/main.cpp
+++ Study/main.cpp
@@ -1,23 +1,22 @@
 #include <iostream>
 
-// 2908번 상수
+// 5622번 다이얼
 
-void reverse(int& n)
+int TransAlp(char* a,int dial[])
 {
-	int tmp = 0;
-	do
-	{
-		tmp = tmp * 10 + n % 10;
-		n /= 10;
-	} while (n);
-	n = tmp;
+	return dial[*a - 65];
 }
 
 int main()
 {
-	int A, B;
-	std::cin >> A >> B;
-	reverse(A);
-	reverse(B);
-	std::cout << (A > B ? A : B);
+	char word[16];
+	char* pw = word;
+	int dial[26] = { 3,3,3,4,4,4,5,5,5,6,6,6,7,7,7,8,8,8,8,9,9,9,10,10,10,10 }, sec = 0;
+	std::cin >> word;
+	while (*pw)
+	{
+		sec += TransAlp(pw,dial);
+		pw++;
+	}
+	std::cout << sec;
 }
