e5e9c1f77e2d37bfb25718c9fa37cadf015cd57b
chnxik <chnxik@gmail.com>
2022-02-27 23:46:43 +0900

    백준 2941번 크로아티아 알파벳

--- Study/main.cpp
+++ Study/main.cpp
@@ -1,22 +1,25 @@
 #include <iostream>
 
-// 5622번 다이얼
-
-int TransAlp(char* a,int dial[])
-{
-	return dial[*a - 65];
-}
+// 2941번 크로아티아 알파벳
 
 int main()
 {
-	char word[16];
-	char* pw = word;
-	int dial[26] = { 3,3,3,4,4,4,5,5,5,6,6,6,7,7,7,8,8,8,8,9,9,9,10,10,10,10 }, sec = 0;
-	std::cin >> word;
-	while (*pw)
+	char str[101];
+	std::cin >> str;
+	int cnt = 1;
+	for (int i = 1; 0 != str[i]; ++i)
 	{
-		sec += TransAlp(pw,dial);
-		pw++;
+
+		if (str[i] == '-' || str[i] == '=')
+		{
+			--cnt;
+			if (1<i && (str[i - 1] == 'z' && str[i - 2] == 'd')) --cnt;
+		}
+		else if (0<i && str[i] == 'j')
+		{
+			if (str[i - 1] == 'l' || str[i - 1] == 'n') --cnt;
+		}
+		++cnt;
 	}
-	std::cout << sec;
+	std::cout << cnt;
 }
