f8f41217e312079b7ae9f017cec66516fe4ec698
chnxik <chnxik@gmail.com>
2022-02-27 18:39:08 +0900

    백준 2675번 문자열 반복

--- Study/main.cpp
+++ Study/main.cpp
@@ -1,24 +1,25 @@
 #include <iostream>
-#include <cstring>
 
-// 10809번 알파벳 찾기
+// 2675번 문자열 반복
+
+using std::cin;
+using std::cout;
 
 int main()
 {
-	char S[101];
-	std::cin >> S;
-	int alphabet[26], tmp;
-	memset(alphabet, -1, sizeof(int) * 26);
-	
-	for (int i = 0; S[i] != 0; ++i)
+	int T, R;
+	char S[21];
+	char* ps;
+	cin >> T;
+	while (T--)
 	{
-		tmp = (int)S[i] - 97;
-		if (-1 == alphabet[tmp]) alphabet[tmp] = i;
-	}
-	
-	for (int i = 0; i < 26; ++i)
-	{
-		if (i) std::cout << " ";
-		std::cout << alphabet[i];
+		cin >> R >> S;
+		ps = S;
+		while (*ps)
+		{
+			for (int i = 0; i < R; ++i) cout << *ps;
+			++ps;
+		}
+		cout << '\n';
 	}
 }
