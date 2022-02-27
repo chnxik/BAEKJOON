c291613d633df47da7f78665dea961ac41eb653f
chnxik <chnxik@gmail.com>
2022-02-27 20:34:29 +0900

    백준 2908번 상수

--- Study/main.cpp
+++ Study/main.cpp
@@ -1,24 +1,17 @@
 #include <iostream>
-#include <string>
 
-// 1152번 단어의 개수
-std::string str;
+// 2908번 상수
+
+void reverse(int &n)
+{
+	n = n / 100 + (n % 100) / 10 * 10 + (n % 10) * 100;
+}
 
 int main()
 {
-	bool check = true;
-	int cnt = 0;
-	std::getline(std::cin, str);
-	std::string::iterator _iter = str.begin();
-	while (_iter != str.end())
-	{
-		if (check && *_iter != ' ')
-		{
-			check = false;
-			++cnt;
-		}
-		if (!check && *_iter == ' ') check = true;
-		++_iter;
-	}
-	std::cout << cnt;
+	int A, B;
+	std::cin >> A >> B;
+	reverse(A);
+	reverse(B);
+	std::cout << (A > B ? A : B);
 }
