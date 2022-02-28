302c8f233505c50f052281d719523860fc75db8c
chnxik <chnxik@gmail.com>
2022-02-28 22:40:20 +0900

    백준 10757번 큰 수 A+B

--- Study/main.cpp
+++ Study/main.cpp
@@ -1,11 +1,52 @@
 #include <iostream>
+#include <string.h>
 
-// 2839번 설탕 배달
+// 10757번 큰 수 A+B
 
 int main()
 {
-	int N;
-	std::cin >> N;
-	if (N == 4 || N == 7) std::cout << -1;
-	else std::cout << N/5 + ((N%5) ? (N % 5) % 2 ? 1 : 2 : 0);
+	std::cout.sync_with_stdio(false);
+	char* A = new char[10001], * B = new char[10001], * res = new char[10002];
+	std::cin >> A >> B;
+	char* pa = &A[(strlen(A)) - 1], * pb = &B[(strlen(B)) - 1], * pc = res;
+	int a, b, r, u = 0;
+	bool ba = true, bb = true;
+	while (true)
+	{
+		a = ba ? *pa - 48 : 0;
+		b = bb ? *pb - 48 : 0;
+		if (ba && pa == A)
+		{
+			a = *pa - 48;
+			ba = false;
+		}
+		else if (ba) --pa;
+		if (bb && pb == B)
+		{
+			b = *pb - 48;
+			bb = false;
+		}
+		else if (bb) --pb;
+		r = a + b + u;
+		u = r / 10;
+		r %= 10;
+		*pc++ = r + 48;
+		if (!ba && !bb)
+		{
+			if(u) *pc++ = u + 48;
+			*pc-- = 0;
+			break;
+		}
+	}
+	while (true)
+	{
+		std::cout << *pc - 48;
+		if (--pc == res)
+		{
+			std::cout << *pc - 48;
+			break;
+		}
+	}
+
+	delete[] A, B, res;
 }
