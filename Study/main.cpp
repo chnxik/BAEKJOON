f0bba691d48e1769af49f183861609ab67adc463
chnxik <chnxik@gmail.com>
2022-03-02 19:23:02 +0900

    백준 1085번 직사각형에서 탈출

--- Study/main.cpp
+++ Study/main.cpp
@@ -1,38 +1,12 @@
 #include <iostream>
-#define SIZE 10001
 
-// 9020번 골드바흐의 추측
-
-int decArr[SIZE] = {0,0,2,};
-
-void SetArr(int Arr[], int size)
-{
-	int i = 3;
-	for (; i < size; i += 2) Arr[i] = i;
-}
-
-void SetDec(int Arr[], int size)
-{
-	for (int i = 2; i * i < size; ++i)
-		if (Arr[i])
-			for (int j = 2; i*j < size; ++j)
-				Arr[i*j] = 0;
-}
+// 1085번 직사각형에서 탈출
 
 int main()
 {
-	SetArr(decArr, SIZE);
-	SetDec(decArr, SIZE);
-	int T, n, hn;
-	std::cin >> T;
-	while (T--)
-	{
-		std::cin >> n;
-		hn = n / 2;
-		while (decArr[hn] + decArr[n - hn] != n)
-		{
-			while(!decArr[--hn]);
-		}
-		std::cout << hn << ' ' << n - hn << '\n';
-	}
+	int x, y, w, h;
+	std::cin >> x >> y >> w >> h;
+	x = x > w / 2 ? w - x : x;
+	y = y > h / 2 ? h - y : y;
+	std::cout << (x > y ? y : x);
 }
