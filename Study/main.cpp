4249fe7a1f4c0cd5cef395f094353d22de9f4f97
chnxik <chnxik@gmail.com>
2022-03-04 21:17:58 +0900

    백준 10870번 피보나치 수 5

--- Study/main.cpp
+++ Study/main.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
 
-// 10872번 팩토리얼
+// 10870번 피보나치 수 5
 
-int factorial(int n)
+int F(int n)
 {
-	if (!n) return 1;
-	return factorial(n - 1) * n;
+	if (!n) return 0;
+	else if (n < 3) return 1;
+	return F(n - 1) + F(n - 2);
 }
 
 int main()
 {
 	int n;
 	std::cin >> n;
-	std::cout << factorial(n);
+	std::cout << F(n);
 }
